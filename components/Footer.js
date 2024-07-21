4b330fbabe91e45b3fe760e2a1cc474fb0a69b68
RasoulAskari <rasoul.merzai123@gmail.com>
2024-07-21 11:39:12 +0500

    changes

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <>
+      <h1>Footer</h1>
+    </>
+  );
+}
---
+++ components/Skill.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <>
+      <h1>Skill</h1>
+    </>
+  );
+}
